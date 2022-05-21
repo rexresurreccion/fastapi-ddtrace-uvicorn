9d53b629200b5aec8369ed12371a5e40b0ddd4e1
rexresurreccion <rexjustin_02@yahoo.com>
2022-05-21 09:09:36 -0500

    Remove test logs

--- webapp/webapp/main.py
+++ webapp/webapp/main.py
@@ -3,7 +3,6 @@ import requests
 import time
 import os
 
-#import structlog
 from pythonjsonlogger import jsonlogger
 from fastapi import FastAPI, BackgroundTasks
 
@@ -12,13 +11,6 @@ app = FastAPI()
 FORMAT = ('%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] '
           '[dd.service=%(dd.service)s dd.env=%(dd.env)s dd.version=%(dd.version)s dd.trace_id=%(dd.trace_id)s dd.span_id=%(dd.span_id)s] '
           '- %(message)s')
-
-#logging.basicConfig(format=FORMAT, filename="/var/log/webapp/python/log.log")
-#log = logging.getLogger(__name__)
-#log.setLevel(logging.DEBUG)
-#structlog_configure()
-#log = structlog.getLogger(__name__)
-#log.bind(group_identifier="grp123")
 
 logHandler = logging.FileHandler(filename='/var/log/webapp/python/log.log')
 formatter = jsonlogger.JsonFormatter('%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] %(message)s')
@@ -31,13 +23,12 @@ log.setLevel(logging.DEBUG)
 
 def write_notification():
     log.info("Background Tast", extra={"client_id": "1123"})
-    response = requests.get(f"http://ec2-18-188-253-48.us-east-2.compute.amazonaws.com:8081/?timestamp={time.time()}")
+    response = requests.get(f"http://ec2-11-111-111-11.us-east-2.compute.amazonaws.com:8081/?timestamp={time.time()}")
 
 
 @app.get("/")
 async def root(background_tasks: BackgroundTasks):
     webapp = {"webapp": "Hello World"}
-    #webapp.update(response.json())
     log.debug("DD Env", extra={"ENVS": os.environ})
     log.info("Happy DD Logging!", extra={"client_id": "1123"})
     log.debug("Debug mode loggging", extra={"client_id": "1123"})
